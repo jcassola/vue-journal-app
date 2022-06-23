1555902b97049e315820bad9df7180f6c9f06738
Elodin <jcassola96@gmail.com>
2022-06-23 15:21:40 -0400

    fake entries and release tag

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,5 +2,24 @@
 
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro animi aliquid totam odit? Unde debitis saepe reprehenderit a ducimus! Ipsa vel rerum, totam aut similique repudiandae nihil placeat consequatur veniam tempora distinctio! Vitae ab nihil ea deserunt incidunt, veritatis animi possimus perspiciatis excepturi culpa quam ipsam! Necessitatibus blanditiis nesciunt incidunt.',
+            picture: null //https://
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, laboriosam alias quae debitis perspiciatis cum necessitatibus aliquid ipsum, aliquam corporis odit harum voluptatibus nobis? Voluptas, sed. Impedit sunt autem nostrum corporis suscipit dignissimos rem mollitia dolores amet perferendis pariatur atque cumque reprehenderit quae aliquid, beatae fugit accusamus ex cum sint.',
+            picture: null //https://
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore sint autem deserunt. Voluptas, soluta nulla. Laborum, iusto qui dolor sit quis iste? Itaque nam ducimus illo explicabo adipisci neque quisquam placeat omnis sed ullam dicta, dolorum iste! Vitae at magni, temporibus aut distinctio neque, repellendus suscipit deleniti laboriosam recusandae quaerat?',
+            picture: null //https://
+        }
+    ]
 })
